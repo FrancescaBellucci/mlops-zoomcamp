29a0b0398630bf91b10af5b0f0d67b04a9bd0ef7
FrancescaBellucci <francesca.bellucci@live.com>
2024-08-19 19:05:07 +0000

    Added s3 as mlflow storage

--- final_project/tests/test_predictions.py
+++ final_project/tests/test_predictions.py
@@ -1,8 +1,16 @@
 from unittest.mock import patch
+import os
 
 import requests
 from deepdiff import DeepDiff
 
+import mlflow
+import pickle
+
+ARTIFACT_PATH = os.getenv("MLFLOW_ARTIFACT_PATH")
+
+download_model("fddbb35346874fd89d03a1558b7a5881","isolation_forest") 
+download_model("b234e2475dc1456385bb7ecbf05f83ef","xgboost") 
 
 def test_prediction_app():
     url = 'http://localhost:9696/predict'
@@ -31,6 +39,16 @@ def test_prediction_app():
 
     assert 'type_changes' not in diff
     assert 'values_changed' not in diff
+    
+def download_model(run_id, model_name):
+    
+    logged_model = f"{ARTIFACT_PATH}/1/{run_id}/artifacts/model/"
+    model = mlflow.pyfunc.load_model(logged_model)
+    
+    model_path = '../models/' + model_name + '.bin'
+    
+    with open(model_path, 'wb') as f_out:
+        pickle.dump(model, f_out)
 
 
 # For offline test
